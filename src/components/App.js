a1a6330d2a9a3e67c70b46cf07babd3eb55f1207
Simon Grandvaux <clement.grandvaux@hotmail.com>
2021-09-16 16:18:13 +0200

    premier commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import logo from '../styles/logo.svg';
+import '../styles/App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default App;
